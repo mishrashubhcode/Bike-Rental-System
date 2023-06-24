df4e363afeb4f8d14df8fa8cc607984685e8bb00
shubham kumar <kumarshubham41515@gmail.com>
2023-06-24 14:48:19 -0400

    Add project files.

---
+++ Bike-Rental-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bike_Rental_System.Startup))]
+namespace Bike_Rental_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
